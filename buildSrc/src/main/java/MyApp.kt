a90d3f39849520fc6f8e0b95a081c8bc299eb455
dongping.wang <wangdongping@meizu.com>
2024-05-09 01:16:03 +0800

    适配一加手机系统图标；增加几个bili图标；修改版本号v0.0.2

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "dev.tang.with.you"
-    const val version = 1
-    const val versionName = "0.0.1"
+    const val version = 2
+    const val versionName = "0.0.2"
 }
