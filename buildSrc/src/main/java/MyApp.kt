fa9656b7c41ebbfb4ca49ca9a148507ebe0da344
dongping.wang <wangdongping@meizu.com>
2024-05-12 23:32:38 +0800

    增加一些新图标；修改版本号为1.0.1alpha

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "dev.tang.with.you"
-    const val version = 3
-    const val versionName = "1.0.0 Alpha"
+    const val version = 4
+    const val versionName = "1.0.1 Alpha"
 }
